db616e7c2b4595bff73292207a8662a6c2067f1c
lilzooty <nickkroop@gmail.com>
2025-09-11 23:40:23 -0600

    random letter 'x' deleted from MainActivity that was messing up the build/run

--- Lecture5InClassPractice/app/src/main/java/com/example/lecture5inclasspractice/MainActivity.kt
+++ Lecture5InClassPractice/app/src/main/java/com/example/lecture5inclasspractice/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier.padding(20.dp)x 
+        modifier = modifier.padding(20.dp)
     )
 }
 
