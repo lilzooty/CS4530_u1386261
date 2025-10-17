88eabf83778c53d6a283ddc1b7b0c0a5f42b5a0b
lilzooty <nickkroop@gmail.com>
2025-10-17 01:46:28 -0600

    implemented CourseDao.kt

--- A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/CourseDao.kt
+++ A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/CourseDao.kt
@@ -1,4 +1,23 @@
-package com.example.a2courseviewereditorapp
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.a2courseviewereditorapp.Course
+import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface CourseDao {
+    @Query("SELECT * FROM course")
+    fun getAllCourses(): Flow<List<Course>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCourse(course: Course)
+
+    @Update
+    suspend fun updateCourse(course: Course)
+
+    @Delete
+    suspend fun deleteCourse(course: Course)
 }
