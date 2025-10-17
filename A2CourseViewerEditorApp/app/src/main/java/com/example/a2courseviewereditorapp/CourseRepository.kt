35f55efefea55f447ad19889b379171abd4b7d90
lilzooty <nickkroop@gmail.com>
2025-10-17 01:49:00 -0600

    implemented CourseRepository.kt

--- A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/CourseRepository.kt
+++ A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/CourseRepository.kt
@@ -1,4 +1,10 @@
-package com.example.a2courseviewereditorapp
+import com.example.a2courseviewereditorapp.Course
+import kotlinx.coroutines.flow.Flow
 
-class CourseRepository {
+class CourseRepository(private val courseDao: CourseDao) {
+    val allCourses: Flow<List<Course>> = courseDao.getAllCourses()
+
+    suspend fun insert(course: Course) = courseDao.insertCourse(course)
+    suspend fun update(course: Course) = courseDao.updateCourse(course)
+    suspend fun delete(course: Course) = courseDao.deleteCourse(course)
 }
