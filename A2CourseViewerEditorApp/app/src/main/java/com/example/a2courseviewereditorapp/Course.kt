936ed811c01baa6d61946fbef80594179f1a6686
lilzooty <nickkroop@gmail.com>
2025-10-17 01:29:18 -0600

    Updated Course.kt for Room. made entity and given primary key

--- A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/Course.kt
+++ A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/Course.kt
@@ -1,6 +1,11 @@
 package com.example.a2courseviewereditorapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "course")
 data class Course(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val department: String,
     val courseNumber: String,
     val location: String
