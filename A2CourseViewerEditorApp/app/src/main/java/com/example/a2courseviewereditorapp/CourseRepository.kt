69398fc1f61b3da89f12e9c781d9fd0c2752599b
lilzooty <nickkroop@gmail.com>
2025-10-17 19:07:00 -0600

    updated names for CourseRepository.kt functions

--- A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/CourseRepository.kt
+++ A2CourseViewerEditorApp/app/src/main/java/com/example/a2courseviewereditorapp/CourseRepository.kt
@@ -1,10 +1,11 @@
 import com.example.a2courseviewereditorapp.Course
+import com.example.a2courseviewereditorapp.CourseDao
 import kotlinx.coroutines.flow.Flow
 
 class CourseRepository(private val courseDao: CourseDao) {
     val allCourses: Flow<List<Course>> = courseDao.getAllCourses()
 
-    suspend fun insert(course: Course) = courseDao.insertCourse(course)
-    suspend fun update(course: Course) = courseDao.updateCourse(course)
-    suspend fun delete(course: Course) = courseDao.deleteCourse(course)
+    suspend fun insertCourse(course: Course) = courseDao.insertCourse(course)
+    suspend fun updateCourse(course: Course) = courseDao.updateCourse(course)
+    suspend fun deleteCourse(course: Course) = courseDao.deleteCourse(course)
 }
