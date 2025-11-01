f5250cb15947ade25cfff0b27fcdbfd663db454f
lilzooty <nickkroop@gmail.com>
2025-10-31 20:14:07 -0600

    implemented FunFactDao.kt

--- A4_FunFactsApplication/app/src/main/java/com/example/a4_funfactsapplication/FunFactDao.kt
+++ A4_FunFactsApplication/app/src/main/java/com/example/a4_funfactsapplication/FunFactDao.kt
@@ -1,4 +1,16 @@
 package com.example.a4_funfactsapplication
 
-class FunFactDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FunFactDao {
+
+    @Query("SELECT * FROM fun_facts ORDER BY id DESC")
+    fun getAllFacts(): Flow<List<FunFact>>
+
+    @Insert
+    suspend fun insertFact(fact: FunFact)
 }
