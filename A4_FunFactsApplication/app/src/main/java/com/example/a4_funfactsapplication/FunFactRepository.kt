f92bf8c16dba5e60134fa6282efbdfb3002f106e
lilzooty <nickkroop@gmail.com>
2025-10-31 21:09:34 -0600

    implemented FunFactRepository.kt

--- A4_FunFactsApplication/app/src/main/java/com/example/a4_funfactsapplication/FunFactRepository.kt
+++ A4_FunFactsApplication/app/src/main/java/com/example/a4_funfactsapplication/FunFactRepository.kt
@@ -1,4 +1,29 @@
 package com.example.a4_funfactsapplication
 
-class FunFactRepository {
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.flow.Flow
+
+class FunFactRepository(
+    private val funFactDao: FunFactDao,
+    private val ktorClient: HttpClient
+) {
+
+    fun getAllFacts(): Flow<List<FunFact>> {
+        return funFactDao.getAllFacts()
+    }
+
+    suspend fun fetchAndStoreFact(): Result<FunFact> {
+        return try {
+            val fact = ktorClient.get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
+                .body<FunFact>()
+
+            funFactDao.insertFact(fact)
+
+            Result.success(fact)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
