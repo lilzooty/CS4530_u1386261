ae356e6627e95db018d581f52d0ae9c05e83fdcc
lilzooty <nickkroop@gmail.com>
2025-10-31 19:28:20 -0600

    implemented FunFact.kt and its room entity

--- A4_FunFactsApplication/app/src/main/java/com/example/a4_funfactsapplication/FunFact.kt
+++ A4_FunFactsApplication/app/src/main/java/com/example/a4_funfactsapplication/FunFact.kt
@@ -1,4 +1,15 @@
 package com.example.a4_funfactsapplication
 
-class FunFact {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Entity(tableName = "fun_facts")
+data class FunFact(
+    val text: String,
+
+    @PrimaryKey(autoGenerate = true)
+    @kotlinx.serialization.Transient
+    val id: Long = 0
+)
