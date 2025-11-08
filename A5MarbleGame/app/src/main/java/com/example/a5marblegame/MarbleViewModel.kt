b3c7b959be2232aa295845c732e7ea66953e6f03
lilzooty <nickkroop@gmail.com>
2025-11-07 17:04:37 -0700

    skeleton implementation of MarbleViewModel.kt

--- A5MarbleGame/app/src/main/java/com/example/a5marblegame/MarbleViewModel.kt
+++ A5MarbleGame/app/src/main/java/com/example/a5marblegame/MarbleViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.a5marblegame
 
-class MarbleViewModel {
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import androidx.lifecycle.ViewModel
+
+class MarbleViewModel : ViewModel(), SensorEventListener{
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+
 }
