84bd64cee315f06d2a2c7cf73b8a31a01b9ca778
lilzooty <nickkroop@gmail.com>
2025-11-07 22:42:42 -0700

    removed unused imports in MainActivity.kt

--- A5MarbleGame/app/src/main/java/com/example/a5marblegame/MainActivity.kt
+++ A5MarbleGame/app/src/main/java/com/example/a5marblegame/MainActivity.kt
@@ -10,16 +10,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     private val viewModel: MarbleViewModel by viewModels()
 
