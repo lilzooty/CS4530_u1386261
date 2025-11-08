b5c6a5ef4d9a2323f6228bee5778a84f5eb93849
lilzooty <nickkroop@gmail.com>
2025-11-07 19:37:05 -0700

    added a bunch of dependencies for compose

--- A5MarbleGame/app/build.gradle.kts
+++ A5MarbleGame/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+
 }
 
 android {
@@ -9,6 +11,9 @@ android {
         version = release(36)
     }
 
+    buildFeatures {
+        compose = true
+    }
     defaultConfig {
         applicationId = "com.example.a5marblegame"
         minSdk = 24
@@ -43,6 +48,22 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+
+
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
